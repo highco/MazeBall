416cd85b2566a01e3155930298b5341d5e5cb4e6
Heiko Weible <h.weible@visual-generation.de>
2014-12-11 12:41:39 +0100

    Added particle system

---
+++ Assets/SortingLayerExposed.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Wooga.Hex4
+{
+	public class SortingLayerExposed : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
