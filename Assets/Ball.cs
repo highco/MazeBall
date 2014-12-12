006a8eed53fdab329f69ae1d228b458bd5c97da7
Heiko Weible <h.weible@visual-generation.de>
2014-12-12 08:51:38 +0100

    Smoother ball rotations

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Ball : MonoBehaviour
 {
+	const float rotationSpeed = 360f; 
 	public int color;
 	App app;
 
@@ -50,12 +51,13 @@ public class Ball : MonoBehaviour
 	}
 
 	Vector3 dir;
+	float targetAngle;
 
 	void Update()
 	{
 		if (rigidbody2D.velocity.magnitude > .1)
 			dir = rigidbody2D.velocity;
 		var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-		transform.localEulerAngles = new Vector3(0, 0, angle - 90);
+		transform.localEulerAngles = new Vector3(0, 0, Mathf.MoveTowardsAngle(transform.localEulerAngles.z, angle - 90, rotationSpeed * Time.deltaTime));
 	}
 }
